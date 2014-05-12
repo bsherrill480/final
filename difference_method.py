70c8b392c40aadcf9cfab8ef9eb4ecb614c3e066
Brian_S <bsherrill9@hotmail.com>
2014-05-11 22:54:46 -0700

    update 2

--- difference_method.py
+++ difference_method.py
@@ -1,21 +1,25 @@
 import numpy as np
 import os
 import cv2
-class difference_method():
-    def BuildListLetters(self):
+class build_normalized_list:
+
+    def __init__(self, pathsToLetters):
         """
-        builds list of alphabet pictures from /home/brian/Pictures/LETTERS/
+        input: takes list of strings (paths) to letters s.t. A is index 0, B is index 1...
+            Images must be contain letters with white background and black letters
+        output: normalized 16x16 letters where black is the background and white are letters
         """
-        dirWithPics = "/home/brian/Pictures/LETTERS/"
-        letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        imagesList = [cv2.imread(dirWithPics + letters[i] + ".jpg", 0) for i in xrange(26)]
-        return imagesList
+        self.imagesList = [self.NormalizeImage( cv2.imread(i, 0) ) for i in pathsToLetters]
+
+    def GetList(self):
+        return self.imagesList
 
     def DisplayImageList(self, imagesList):
         """
         debuggin to examine images
         """
         for i in imagesList:
+            print i
             cv2.imshow('image',i)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
@@ -24,35 +28,54 @@ class difference_method():
         """
         turns whites to blacks and Normalizes pictures to 8x8 or 16x16 (im still debating)
         """
+        image = self.InvertImage(image)
         colSum = np.sum(image, axis = 0) #columns
         rowSum = np.sum(image, axis = 1) #rows
-        colStart, colEnd = self.begginingEndIndex(colSum)
-        rowStart, rowEnd = self.begginingEndIndex(rowSum)
-        image = image[]
+        sumTot = np.sum(colSum)
+        tolerance = .005 #percent tolerance
+        colStart, colEnd = self.BegginingEndIndex(colSum, tolerance)
+        rowStart, rowEnd = self.BegginingEndIndex(rowSum, tolerance)
+        return cv2.resize(image[rowStart : rowEnd+1, colStart : colEnd+1], (16, 16)) #+1 to end because slicing is [inclusive, exclusive]
 
     def InvertImage(self, image):
         allWhite = np.ndarray(shape = image.shape, dtype=np.uint8)
         allWhite[:] = np.uint8(255)
         return cv2.subtract(allWhite, image)
 
-    def BegginingEndIndex(self, array):
+    def BegginingEndIndex(self, array, tolerance):
         """
-        NO TOLLERANCE SET. change for tollerance if necesary
+        tolerance is contained in (0,1) % of totalTot
         """
+        toleranceNum = np.sum(array)*tolerance
         begin = 0
         end = array.size - 1 #zero based indexing
-        inLetter = not (array[0] == 0) #test to see if we're starting in letter
+        inLetter = not (array[0] < toleranceNum) #test to see if we're starting in letter
         for i in xrange(array.size):
-            if array[i] > 0 and (not inLetter):
+            if array[i] > toleranceNum and (not inLetter):
                 begin = i
                 inLetter = True
-            if array[i] == 0 and inLetter:
+            if array[i] < toleranceNum and inLetter:
                 end = i
                 break
+        #print "array size: " + str(array.size)
+        #print "array: " + str(array)
+        #print "being, end: " + str((begin, end))
         return (begin, end)
+
+class PatternMatcher:
+    def __init__(self, list, ):
 
 
 if __name__ == "__main__":
-    dm = difference_method()
-    imagesList = dm.BuildListLetters()
-    dm.DisplayImageList(imagesList)
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    listOfDirsWithLetters = ["/home/brian/Pictures/LETTERS/"] #different directories
+    listOfNormalizedLetters = []
+    for dirWithPics in listOfDirsWithLetters:
+        pathToLetters = [dirWithPics + letters[i] + ".jpg" for i in xrange(26)]#generates list of paths to each letter
+        listOfNormalizedLetters.append(build_normalized_list(pathToLetters).GetList())
+    #display letters
+    for j in listOfNormalizedLetters:
+        for i in j:
+            cv2.imshow('image',i)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
