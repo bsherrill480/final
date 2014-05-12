4ce188db2a7ee8ebf8442585b2c077a2c3b000ad
Brian_S <bsherrill9@hotmail.com>
2014-05-12 01:17:32 -0700

    update 3

--- difference_method.py
+++ difference_method.py
@@ -3,26 +3,20 @@ import os
 import cv2
 class build_normalized_list:
 
-    def __init__(self, pathsToLetters):
+    def __init__(self, pathsToLetters, debug = False):
         """
         input: takes list of strings (paths) to letters s.t. A is index 0, B is index 1...
             Images must be contain letters with white background and black letters
         output: normalized 16x16 letters where black is the background and white are letters
         """
+        self.debugs = debug
+        if debug:
+            print "debug on"
         self.imagesList = [self.NormalizeImage( cv2.imread(i, 0) ) for i in pathsToLetters]
 
     def GetList(self):
         return self.imagesList
 
-    def DisplayImageList(self, imagesList):
-        """
-        debuggin to examine images
-        """
-        for i in imagesList:
-            print i
-            cv2.imshow('image',i)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
 
     def NormalizeImage(self, image):
         """
@@ -33,8 +27,8 @@ class build_normalized_list:
         rowSum = np.sum(image, axis = 1) #rows
         sumTot = np.sum(colSum)
         tolerance = .005 #percent tolerance
-        colStart, colEnd = self.BegginingEndIndex(colSum, tolerance)
-        rowStart, rowEnd = self.BegginingEndIndex(rowSum, tolerance)
+        colStart, colEnd = self.BegginingEndIndex2(colSum, tolerance)
+        rowStart, rowEnd = self.BegginingEndIndex2(rowSum, tolerance)
         return cv2.resize(image[rowStart : rowEnd+1, colStart : colEnd+1], (16, 16)) #+1 to end because slicing is [inclusive, exclusive]
 
     def InvertImage(self, image):
@@ -42,40 +36,83 @@ class build_normalized_list:
         allWhite[:] = np.uint8(255)
         return cv2.subtract(allWhite, image)
 
-    def BegginingEndIndex(self, array, tolerance):
+    def BegginingEndIndex2(self, array, tolerance):
         """
         tolerance is contained in (0,1) % of totalTot
         """
         toleranceNum = np.sum(array)*tolerance
+        if self.debugs:
+            print "tol num "+str(toleranceNum)
+            print array
         begin = 0
         end = array.size - 1 #zero based indexing
-        inLetter = not (array[0] < toleranceNum) #test to see if we're starting in letter
         for i in xrange(array.size):
-            if array[i] > toleranceNum and (not inLetter):
+            if array[i] > toleranceNum:
                 begin = i
-                inLetter = True
-            if array[i] < toleranceNum and inLetter:
+                break
+        i = end
+        while(i >= 0 ):
+            if array[i] > tolerance:
                 end = i
                 break
-        #print "array size: " + str(array.size)
-        #print "array: " + str(array)
-        #print "being, end: " + str((begin, end))
+            i = i-1
+        if self.debugs:
+            print (begin, end)
+
         return (begin, end)
 
-class PatternMatcher:
-    def __init__(self, list, ):
+class display_images:
+    def display(self, thing):
+        if isinstance(thing, list):
+            for i in thing:
+                cv2.imshow('image',i)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+        else:
+            cv2.imshow('image',thing)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
+class pattern_matcher:
+    def __init__(self, listOfImages, img):
+        self.image = img
+        self.listOfImages = listOfImages
+        diff = self.differenceArray()
+        min = diff[0]
+        self.index = 0
+        print diff
+        for i in xrange(len(listOfImages)):
+            if diff[i] < min:
+                min = diff[i]
+                self.index = i
+    def differenceArray(self):
+        return [np.sum(cv2.subtract(i, self.image)) for i in self.listOfImages]
 
+    def getMatch(self):
+        return self.index
 if __name__ == "__main__":
+    #build lists of letter
     letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     listOfDirsWithLetters = ["/home/brian/Pictures/LETTERS/"] #different directories
     listOfNormalizedLetters = []
     for dirWithPics in listOfDirsWithLetters:
         pathToLetters = [dirWithPics + letters[i] + ".jpg" for i in xrange(26)]#generates list of paths to each letter
         listOfNormalizedLetters.append(build_normalized_list(pathToLetters).GetList())
+    #normalize hand drawn A
+    Apath = "/home/brian/Pictures/A_test.jpg"
+    ANorm = build_normalized_list([Apath], True).GetList()[0]
+    d = display_images()
+    print ANorm
+    d.display(ANorm)
+    #d.display(listOfNormalizedLetters[0])
+    #A = build_normalized_list(listOfNormalizedLetters)#wtf is this?
+    indexOfMatch = pattern_matcher(listOfNormalizedLetters[0], ANorm).getMatch()
+    print "Anorm was matched to: " + str(letters[indexOfMatch])
+    '''
     #display letters
     for j in listOfNormalizedLetters:
         for i in j:
             cv2.imshow('image',i)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
+    '''
